a8a1b2c4d663a6c614f33c37a1fc5865687edcb1
mcadamskiy <mcadamskiy@gmail.com>
2024-10-11 20:47:46 +0500

    Task3 is ended

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Answer{
+    static void Sifra(int a){
+        int a1, a2;
+        a1 = a/10;
+        a2 = a%10;
+        if(a1>a2){
+            Console.WriteLine(a1);
+        }
+        else Console.WriteLine(a2);
+    }
+    static void Main(){
+        int a = Convert.ToInt32(Console.ReadLine());
+        Sifra(a);
+    }
+}
