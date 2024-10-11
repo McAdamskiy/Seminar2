c0922a6ba162c03b208c9cf7c921dcb41a6e4101
mcadamskiy <mcadamskiy@gmail.com>
2024-10-11 21:01:41 +0500

    task4 is ended

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Answer{
+    static void Main(){
+        string input = Console.ReadLine();
+        string result = string.Join(", ", input.ToCharArray());
+        Console.WriteLine(result);
+    }
+}
