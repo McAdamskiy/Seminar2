91fb38f06480387616648a21cd5be91591f185d1
mcadamskiy <mcadamskiy@gmail.com>
2024-10-11 19:07:16 +0500

    task1 is ended

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Answer
+{
+    static void Checking(int a){
+        if(a % 7 == 0 && a% 23 == 0){
+            Console.WriteLine("Da");
+        }
+        else Console.WriteLine("Net");
+    }
+
+    static public void Main(){
+        Console.Write("a= ");
+        int a = Convert.ToInt32(Console.Read());
+        Checking(a);
+    }
+}
